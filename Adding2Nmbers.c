d70cf1a6296b2631c513440594f1c8e696b76fde
Nishil Sheth <88768151+sheth-nishil@users.noreply.github.com>
2025-08-08 10:43:24 +0530

    Adding two numbers

---
+++ Adding2Nmbers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main () {
+    int n1, n2,sum;
+    printf("First Number: ");
+    scanf("%d", &n1);
+    printf("Second number: ");
+    scanf("%d", &n2);
+    sum = n1 + n2;
+    printf("This is my sum: %d", sum);
+    return 0;
+}
